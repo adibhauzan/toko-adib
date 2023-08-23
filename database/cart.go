cb64b1b0c8ffb894867dcbb80c562eb783161e04
Adib Hauzan <adibhauzan48@gmail.com>
2023-08-23 12:56:12 +0700

    settup controllers

--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,29 @@
 package database
+
+import "errors"
+
+var (
+	ErrCantFindProduct = errors.New("can't find the product")
+	ErrCantDecodeProducts = errors.New("can't find the product")
+	ErrUserIdIsNotValid = errors.New("this user is not a valid")
+	ErrCantUpdateUser = errors.New("can't add this product")
+	ErrCantRemoveItemCart = errors.New("can't remove this item from the cart")
+	ErrCantGetItem = errors.New("was unable to get the item from the cart")
+	ErrCantBuyCartItem = errors.New("cannot update the purchase")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveCartItem() {
+
+}
+
+func BuyItemFromCart() {
+
+}
+
+func InstantBuyer() {
+
+}
--- database/databasetup.go
+++ database/databasetup.go
@@ -1,1 +1,55 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBset() *mongo.Client {
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("failed to connect to mongodb (:")
+	}
+	fmt.Println("Sucssefully connected to mongodb")
+
+	return client
+
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collectionName *mongo.Collection = client.Database("Toko Adib").Collection(collectionName)
+	return Collection
+
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+
+	var productCollection *mongo.Collection = client.Database("Toko Adib").Collection(collectionName)
+	
+	return productCollection
+
+}
+
+
